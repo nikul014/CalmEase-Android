8210063838b93d4cb66b49d5b8d83011af3a1caf
Dhruvi Akhilesh Shah <dh368867@dal.ca>
2024-11-24 11:06:52 -0400

    path update in dashboard screen and the main navigation

--- app/src/main/java/com/example/calmease/AppNavigation.kt
+++ app/src/main/java/com/example/calmease/AppNavigation.kt
@@ -1,16 +1,18 @@
 package com.example.calmease
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.calmease.enums.SharedPrefKeys
 import com.example.calmease.ui.screen.about.AboutScreen
-import com.example.calmease.ui.screen.breathing.BreathingScreen
+import com.example.calmease.ui.screen.breathing.BreathingCategoriesScreen
 import com.example.calmease.ui.screen.contact.ContactScreen
 import com.example.calmease.ui.screen.dashboard.DashboardScreen
 import com.example.calmease.ui.screen.login.LoginScreen
 import com.example.calmease.ui.screen.signup.SignupScreen
+import com.example.calmease.viewmodel.BreathingViewModel
 
 @Composable
 fun AppNavigation() {
@@ -27,6 +29,10 @@ fun AppNavigation() {
         composable("dashboard") { DashboardScreen() }
         composable("about") { AboutScreen(navController) }
         composable("contact") { ContactScreen(navController) }
-        composable("breathing") { BreathingScreen() }
+        composable("breathing_categories") {
+            val viewModel: BreathingViewModel = viewModel()
+            BreathingCategoriesScreen(viewModel = viewModel, navController = navController)
+        }
+
     }
 }
